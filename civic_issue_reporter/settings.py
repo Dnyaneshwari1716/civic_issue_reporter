d932d0b693a6b7e620b1b35cd6f6200364fafbb7
Dnyaneshwari1716 <jadhawardnyaneshwari@gmail.com>
2025-04-30 19:48:38 +0530

    Fix email settings and use env variables

--- civic_issue_reporter/settings.py
+++ civic_issue_reporter/settings.py
@@ -136,7 +136,7 @@ EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
 EMAIL_PORT = os.getenv('EMAIL_PORT', 587)
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_APP_PASSWORD')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
 
 # Gemini API
